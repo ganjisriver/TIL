7baa91ff09de30fbcbbf90012f916aae9f42154e
ganjisriver <ganjisriver18@gmail.com>
2022-10-01 23:53:22 +0900

    221001

---
+++ free_problem/BOJ_11720_B4.py
@@ -0,0 +1,6 @@
+N = int(input())
+S = input()
+answer = 0
+for i in range(N):
+    answer += int(S[i])
+print(answer)
--- swea_IM_advanced/swea_backtrack/practice_three.py
+++ swea_IM_advanced/swea_backtrack/practice_three.py
@@ -1,10 +1,13 @@
 # def preorder(idx):
 #     predorder
 
+
 V, E = map(int, input().split())
 E_list = list(map(int, input().split()))
 
-for i in range(E):
+for i in range(E//2):
     parent, child = E_list[2*i], E_list[2*i+1]
+    left = [0]*(V+1)
+    right = [0]*(V+1)
 
 
