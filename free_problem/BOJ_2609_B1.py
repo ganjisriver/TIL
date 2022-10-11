fec3d5b840b62c49654badd38b1a91bbc6f6ebb3
ganjisriver <ganjisriver18@gmail.com>
2022-10-11 18:52:49 +0900

    221011

---
+++ free_problem/BOJ_2609_B1.py
@@ -0,0 +1,14 @@
+A, B = map(int,input().split())
+
+def gcd(a, b) :
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+def lcm(a, b) :
+    g = gcd(a, b)
+    return int(g*(a/g)*(b/g))
+
+print(gcd(A, B))
+print(lcm(A, B))
