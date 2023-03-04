36f257960194d8fb25f33506578ed7d87934532d
ganjisriver <ganjisriver18@gmail.com>
2023-03-04 19:46:23 +0900

    230304

--- just_algorithm/cori_study/23_02_5th/프로그래머스 빛의 경로 사이클.py
+++ just_algorithm/cori_study/23_02_5th/프로그래머스 빛의 경로 사이클.py
@@ -1,4 +1,8 @@
 def solution(grid):
+    rl = len(grid)
+    cl = len(grid[0])
+    left_dict = {1:3, 2:4, 3:2, 4:1}
+    right_dict = {1:4, 2:3, 3:2, 4:1}
     dr = [0, -1, 1, 0, 0]
     dc = [0, 0, 0, -1, 1]
     answer = []
@@ -7,17 +11,21 @@ def solution(grid):
         for j in range(len(grid[i])):
             dict_setting[(i, j)] = []
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            visited_dict = dict_setting
-            for k in range(1, 5):
-                starting_point = grid[i][j]
-                while True:
+    for i in range(1, 5):
+        x, y = 0
+        while True:
+            dx = x + dr
+            dy = y + dc
+            if grid[i][j] == 'L':
+
+            elif grid[i][j] == 'R':
+
+            else:
+
+            for i in range(len(dict_setting[(x, y)])):
+                if dict_setting[(x,y)][i] =
 
 
-
-
-    answer.sort(reverse=True)
     return dict_setting
 
 grid1 = ["SL", "LR"]
