eeb9a94fee6da16d32cbb6506441d885c9b6a4ec
ganjisriver <ganjisriver18@gmail.com>
2023-03-07 12:23:39 +0900

    230307;

---
+++ just_algorithm/cori_study/23_03_1th/프로그래머스 동굴 탐험.py
@@ -0,0 +1,10 @@
+def solution(n, path, order):
+    answer = True
+    # path.sort(key=lambda x:x[0])
+    path_dict = dict()
+    for i in range(len(path)):
+        for j in range(2):
+            if j == 0:
+                
+
+    return answer
