6cd468ec8a9d50e6b4d8ed2e847cfa8dcf319cf2
ganjisriver <ganjisriver18@gmail.com>
2023-04-05 17:28:13 +0900

    230405

--- 알고리즘/23_04_1st/프로그래머스 스티커 모으기(2).py
+++ 알고리즘/23_04_1st/프로그래머스 스티커 모으기(2).py
@@ -1,9 +1,13 @@
-def solution(stikcer):
+def solution(sticker):
     answer = 0
-    visited = set()
-    dp = [0]*(len(stikcer)+1)
-    for i in range(len(stikcer)):
+    sticker_lenth = len(sticker)
+    if sticker_lenth == 1:
+        return sticker[0]
+
+    stikcer_dp = [[0 for _ in range(sticker_lenth)] for _ in range(2)]
+    for i in range(2, len(sticker)):
         off_sticker = 0
+
     return answer
 
 sticker1 = [14, 6, 5, 11, 3, 9, 2, 10]
