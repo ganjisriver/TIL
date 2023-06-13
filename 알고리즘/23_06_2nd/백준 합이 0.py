44b1e1a67566ed338af240ce0c9251b45baff4bb
ganjisriver <ganjisriver18@gmail.com>
2023-06-13 20:17:34 +0900

    230613

---
+++ 알고리즘/23_06_2nd/백준 합이 0.py
@@ -0,0 +1,17 @@
+# 1차시도 메모리 초과
+# import sys
+# from itertools import combinations
+# input = sys.stdin.readline
+# N = int(input())
+# arr = list(map(int, input().split()))
+# cnt = 0
+# arr = list(combinations(arr, 3))
+# for i in arr:
+#     if sum(i) == 0:
+#         cnt += 1
+# print(cnt)
+
+
+
+
+
