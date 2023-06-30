dafb264105041e41e46fedaf48feb376d87bbfd5
ganjisriver <ganjisriver18@gmail.com>
2023-06-30 22:34:11 +0900

    230630

--- 알고리즘/23_06_3rd/백준 링크와 스타트.py
+++ 알고리즘/23_06_3rd/백준 링크와 스타트.py
@@ -1,4 +1,6 @@
 from itertools import combinations
+import sys
+input = sys.stdin.readline
 N = int(input())
 arr = []
 team_numbers_list = []
@@ -6,19 +8,31 @@ start_team_member_list = []
 link_team_member_list = []
 order_list = [i+1 for i in range(N)]
 
-for i in range(N):
-    score_list = list(map(int, input().split()))
-    arr.append(score_list)
+score_list = [list(map(int, input.split())) for _ in range(N)]
 for i in range(2, N):
     first, second = i, N-i
     if first > second:
         break
     else:
-        team_numbers_list.append((first, second))
+        team_numbers_list.append(first)
 
-for first, second in team_numbers_list:
-    
+for first in team_numbers_list:
+    order_combi_list = list(combinations(order_list, first))
+    res = 99999999999999
+    for t in range(len(order_combi_list)):
+        order_combi_list[t] = set(order_combi_list[t])
+        start_power = 0
+        link_power = 0
+        for i in range(N//2):
+            for j in range(N//2):
+                if i in order_combi_list[t] and j in order_combi_list[t]:
+                    start_power += arr[i][j]
+                elif i not in order_combi_list[t] and j not in order_combi_list[t]:
+                    link_power += arr[i][j]
+        if res > abs(start_power - link_power):
+            res = abs(start_power - link_power)
+        if res == 0:
+            break
+print(res)
 
-order_combi_list = list(combinations(order_list, 3))
-for i in range(len(order_combi_list)):
-    order_combi_list[i] = set(order_combi_list[i])
+
