1f2f75bcb368dfce7d81c9095e68f809f6ea4d78
ganjisriver <ganjisriver18@gmail.com>
2023-07-05 15:58:27 +0900

    230705

--- 알고리즘/23_07_1st/백준 숫자 연결하기.py
+++ 알고리즘/23_07_1st/백준 숫자 연결하기.py
@@ -3,16 +3,38 @@ input=sys.stdin.readline
 N, K = map(int, input().split())
 default_N = str(N)
 flag = True
+answer = 1
 cnt = 1
-while flag:
-    if N % K == 0:
-        flag = False
-    else:
-        N = str(N)
-        N = N+default_N
-        N = int(N)
-        cnt += 1
-    if cnt > 10000:
-        flag = False
-        cnt = -1
-print(cnt)
+if K == 1 or N % K == 0:
+    flag = False
+else:
+    mok = 10000 // K
+    while cnt < mok:
+        if N % K == 0:
+            flag = False
+            break
+        else:
+            N = str(N)
+            N = N + default_N
+            N = int(N)
+            cnt += 1
+if flag:
+    print("-1")
+else:
+    print(cnt)
+
+# import sys
+# input=sys.stdin.readline
+# N, K = map(int, input().split())
+# default_N = str(N)
+# flag = True
+# cnt = 1
+# if N % K == 0:
+#     flag = False
+#     cnt = 1
+# elif K % 10 == 0:
+#     flag = False
+#     cnt = -1
+# else:
+#     cnt = K
+# print(cnt)
