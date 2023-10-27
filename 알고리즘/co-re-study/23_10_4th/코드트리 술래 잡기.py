a40af525ddac397ac125f43b8fd24563d7f673b0
ganjisriver <ganjisriver18@gmail.com>
2023-10-27 21:52:11 +0900

    231027

---
+++ 알고리즘/co-re-study/23_10_4th/코드트리 술래 잡기.py
@@ -0,0 +1,15 @@
+n, m, h, k = map(int, input().split())
+arr = [[[] for _ in range(n)] for _ in range(n)]
+domang_idx_list = []
+sulae_idx = [n//2, n//2, 0]
+for i in range(m):
+    x, y, move_type = map(int ,input().split())
+    arr[x][y].append(move_type)
+    domang_idx_list.append([x, y, move_type])
+
+for i in range(h):
+    x, y = map(int, input().split())
+    arr[x][y].append(3)
+
+sulae_idx = [n//2, n//2]
+arr[sulae_idx[0]][sulae_idx[1]] = 9
